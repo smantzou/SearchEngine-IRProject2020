2792cdd4f0f7030ab3859e9309f93d81224fcad3
smantzou <32647363+smantzou@users.noreply.github.com>
2020-12-07 12:12:32 +0200

    Add files via upload

---
+++ general.py
@@ -0,0 +1,61 @@
+import os
+
+
+# Each website you crawl is a separate project
+
+def create_project_dir(directory):
+    if not os.path.exists(directory):
+        print("Creating project " + directory)
+        os.makedirs(directory)
+
+
+# Creates new file
+
+def write_file(path, data):
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
+
+
+# Create queue and crawled files
+
+def create_data_files(project_name, base_url):
+    queue = project_name + '/queue.txt'
+    crawled = project_name + '/crawled.txt'
+    if not os.path.isfile(queue):
+        write_file(queue, base_url)
+    if not os.path.isfile(crawled):
+        write_file(crawled, '')
+
+
+# Add data onto an existing file
+
+def append_to_file(path, data):
+    with open(path, 'a', encoding="utf-8", errors="ignore") as file:
+        file.write(data + '\n')
+
+
+# Delete the contents of a file
+
+def delete_file_contents(path):
+    with open(path, 'w'):
+        pass
+
+
+# Read a file and convert each line to set item
+
+def file_to_set(file_name):
+    results = set()
+    with open(file_name, 'rt', encoding="UTF-8", errors="ignore") as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
+        return results
+
+
+# Iterate through a set , each item will be a new line in the file
+
+
+def set_to_file(links, file):
+    delete_file_contents(file)
+    for link in sorted(links):
+        append_to_file(file, link)
