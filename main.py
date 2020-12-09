f1e7b5256ef576f35af9c60f16bf80a73ba1e987
smantzou <smantzou@github.com>
2020-12-09 20:00:39 +0200

    update on crawler :main takes arguements starting page number of crawls, keep or not previous files and threads number of jobs per thread , program exits after complete a number of crawls

--- main.py
+++ main.py
@@ -3,35 +3,38 @@ from queue import Queue
 from spider import Spider
 from domain import *
 from general import *
-
-PROJECT_NAME = 'atmo'
-HOMEPAGE = 'https://stats.stackexchange.com/'
-DOMAIN_NAME = get_domain_name(HOMEPAGE)
-QUEUE_FILE = PROJECT_NAME + '/queue.txt'
-CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
-NUMBER_OF_THREADS = 4
-queue = Queue()
-Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+import sys
+import os
+import time
 
 
 #  create a worker set(will die with main)
 def create_workers():
     for _ in range(NUMBER_OF_THREADS):
-        t = threading.Thread(target=work)
+        t = threading.Thread(target=pre_work)
         t.daemon = True
         t.start()
 
 
+def pre_work():
+    work()
+    time.sleep(3)
+    os._exit(0)
+
+
 # do the next job in the queue
 
-def work():
-    while True:
+def work(i=0):
+    while i < jobs_per_thread:
         url = queue.get()
         Spider.crawl_page(threading.Thread().name, url)
         queue.task_done()
+        i += 1
 
 
-# each queued link is a new job
+    # each queued link is a new job
+
+
 def create_jobs():
     for link in file_to_set(QUEUE_FILE):
         queue.put(link)
@@ -47,5 +50,17 @@ def crawl():
         create_jobs()
 
 
-create_workers()
-crawl()
+if __name__ == '__main__':
+    PROJECT_NAME = 'stackoverflow'
+    HOMEPAGE = sys.argv[1]
+    DOMAIN_NAME = get_domain_name(HOMEPAGE)
+    QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+    CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+    NUMBER_OF_THREADS = int(sys.argv[4])
+    MAX_NUMBER_OF_CRAWLS = int(sys.argv[2])
+    keep_old_files = bool(sys.argv[3])
+    jobs_per_thread = MAX_NUMBER_OF_CRAWLS / NUMBER_OF_THREADS
+    queue = Queue()
+    Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+    create_workers()
+    crawl()
